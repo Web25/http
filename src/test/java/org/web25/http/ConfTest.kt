9a6c2ca339bf1e079fe33532c6c58c077530da3f
doompickaxe <kwatzal@gmail.com>
2017-02-14 17:29:20 +0100

    added test for loading configuration

--- src/test/java/org/web25/http/ConfTest.kt
+++ src/test/java/org/web25/http/ConfTest.kt
@@ -1,2 +1,21 @@
-package org.web25.http.transport.http2
+package org.web25.http
 
+import org.junit.Test
+import org.web25.http.util.handler
+import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class ConfTest {
+
+    @Test
+    fun loadServerFromConf(){
+        val http = Http()
+        val server = http.server(File(javaClass.classLoader.getResource("server.conf").toURI())).start()
+        val response = http.get("http://localhost:8080/").response()
+        assertNotNull(server)
+        assertNotNull(response)
+        assertEquals(404, response.statusCode())
+    }
+
+}
